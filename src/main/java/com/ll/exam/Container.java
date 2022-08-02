e0e4c713807043cdcec3361e83db3108e404675e
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-08-02 15:15:23 +0900

    Add : Autowired 테스트의 편리성을 위해 Ut 클래스에 reflection 추가

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -2,6 +2,7 @@ package com.ll.exam;
 
 import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.Repository;
 import com.ll.exam.annotation.Service;
 import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.article.service.ArticleService;
@@ -17,6 +18,7 @@ public class Container {
     static {
         objects = new HashMap<>();
 
+        scanRepositories();
         scanServices();
         scanControllers();
         initObject();
@@ -57,6 +59,13 @@ public class Container {
     private static void scanControllers() {
         Reflections ref = new Reflections("com.ll.exam");
         for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
+            objects.put(cls, Ut.cls.newObj(cls, null));
+        }
+    }
+
+    private static void scanRepositories(){
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Repository.class)) {
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
     }
--- src/main/java/com/ll/exam/Ut.java
+++ src/main/java/com/ll/exam/Ut.java
@@ -2,10 +2,26 @@ package com.ll.exam;
 
 import com.ll.exam.article.controller.ArticleController;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Ut {
+    public static class reflection {
+        public static <T> T getFieldValue(Object o, String fieldName, T defaultValue) {
+            Field field = null;
 
+            try {
+                field = o.getClass().getDeclaredField(fieldName);
+                field.setAccessible(true);
+                return (T)field.get(o);
+            } catch (NoSuchFieldException e) {
+                return defaultValue;
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
     public static class cls {
 
         public static <T>T newObj(Class<T> cls, T defaultValue) {
